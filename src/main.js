d8a9cc3133de818d80d6af024b9257fe7a77abf7
edgar-bonilla <esgareliecer32@gmail.com>
2024-12-17 21:24:11 -0600

    cors

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import router from './router';
 const app = createApp(App);
 
 
-app.config.globalProperties.$url = import.meta.env.VITE_API_URL || 'http://localhost:8888';
+
+
+
+app.config.globalProperties.$url =  'https://catalogocarsredis.netlify.app';
 
 app.use(router).mount('#app');
